dd023ce1e66d9fb3d4520aa6881e658cc7f50b02
Bart Slangewal <bartslangewal@hotmail.com>
2012-11-27 15:02:41 +0100

    User class gemaakt

---
+++ Wielwijk/src/wielwijk/User.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wielwijk;
+
+/**
+ *
+ * @author Bart
+ */
+public class User {
+
+    private String name;
+    private String password;
+    private String birthdate;
+    private boolean active;
+    private int picture;
+    private boolean board;
+    
+    
+}
+//user class, spreek voor zich
+
+}
