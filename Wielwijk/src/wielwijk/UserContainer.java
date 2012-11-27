645d657eabf2aee1cacd1b6f610689a7b4aa5fbd
Yannick Verhoog <ylverhoog@gmail.com>
2012-11-27 17:49:00 +0100

    Fixed

--- Wielwijk/src/wielwijk/UserContainer.java
+++ Wielwijk/src/wielwijk/UserContainer.java
@@ -3,8 +3,37 @@ package wielwijk;
 
 public class UserContainer {
     
-    public void AddUser(String name, String password,String birthdate, boolean active, int picture, boolean board){
-        User newuser = new User(name, password, birthdate, active, picture, board);
+    public void AddBoard(String name, String password,String birthdate, boolean active, int picture, boolean board){
+        Board newBoard = new Board(name, password, birthdate, active, picture);
+        
+        
+        //database create verhaaltje komt hier
     }
+    
+    public void AddMember(String name, String password,String birthdate, boolean active, int picture, boolean board){
+        Member newMember = new Member(name, password, birthdate, active, picture);
+        
+        
+        //database create verhaaltje komt hier
+    }
+    
+    
+    public void RemoveUser(User user){
+        //datebase remove user verhaalte
+    }
+    
+    public void SuspendUser(User user){
+        //datebase user.active verhaaltje
+    }
+    
+    public void UnSuspenduser(User user){
+        //datebase user.activer verhaaltje
+    }
+    
+    public void FindUser(User user){
+        //database zoek user verhaaltje
+    }
+    
+    
     
 }
