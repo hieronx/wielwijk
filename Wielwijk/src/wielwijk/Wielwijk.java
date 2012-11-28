edbf85b3ca425a93d3bde33a868053a425d86fb7
Jeroen Offerijns <jeroen@offerijns.nl>
2012-11-28 15:11:09 +0100

    Test voor toevoegen gebruiker

--- Wielwijk/src/wielwijk/Wielwijk.java
+++ Wielwijk/src/wielwijk/Wielwijk.java
@@ -14,19 +14,6 @@ public class Wielwijk {
      */
     public static void main(String[] args) {
         UserContainer uc = new UserContainer();
-        List res = uc.findUser("off");
-        
-        for (int i = 0; i < res.size(); i++) {
-            Map<String, Object> map = (HashMap<String, Object>) res.get(i);
-
-            Iterator it = map.entrySet().iterator();
-            while (it.hasNext()) {
-                Map.Entry pairs = (Map.Entry)it.next();
-                System.out.println(pairs.getKey() + " = " + pairs.getValue());
-                it.remove(); // avoids a ConcurrentModificationException
-            }
-            
-            System.out.println("----------");
-        }
+        uc.addMember("Pietje", "banaan", "", "Verweggistan", "");
     }
 }
