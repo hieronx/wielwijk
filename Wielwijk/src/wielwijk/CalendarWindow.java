3cf75acf2f310dde8a8e0004a8b2fa10863895e3
Jeroen Offerijns <jeroen@offerijns.nl>
2012-11-29 17:29:10 +0100

    Kalender verbeterd

--- Wielwijk/src/wielwijk/CalendarWindow.java
+++ Wielwijk/src/wielwijk/CalendarWindow.java
@@ -14,26 +14,45 @@ import javax.swing.*;
  */
 public class CalendarWindow extends JFrame {
     
-    JPanel container = new JPanel();
-    JPanel panel;
-    JLabel label;
+    JPanel calendar = new JPanel();
+    JPanel panel, item, layout, layout2;
+    JLabel month, label, index;
     
     public CalendarWindow() {
-        container.setLayout(new GridLayout(7, 5));
-        container.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
+        getContentPane().setLayout(new BorderLayout());
         
-        for (int i = 0; i < 35; i++) {
+        JPanel layout = new JPanel();
+        JLabel month = new JLabel();
+        month.setText("November");
+        month.setFont(month.getFont().deriveFont(26.0f));
+        layout.add(month);
+        add(layout, BorderLayout.NORTH);
+        
+        calendar.setLayout(new GridLayout(7, 5));
+        calendar.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
+        
+        for (int i = 1; i <= 31; i++) {
+            JPanel item = new JPanel(new BorderLayout());
+            
+            JPanel layout2 = new JPanel();
+            JLabel index = new JLabel();
+            index.setText(Integer.toString(i));
+            layout2.add(index);
+            layout2.add(Box.createRigidArea(new Dimension(30, 0)));
+            item.add(layout2, BorderLayout.WEST);
+            
             JLabel label = new JLabel();
-            label.setText("Activiteit " + (i+1));
+            label.setText("Activiteit");
+            item.add(Box.createRigidArea(new Dimension(0, 60)), BorderLayout.CENTER);
+            item.add(label, BorderLayout.CENTER);
             
             JPanel panel = new JPanel();
             panel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-            panel.add(Box.createRigidArea(new Dimension(0, 60)));
-            panel.add(label);
+            panel.add(item);
             
-            container.add(panel);
+            calendar.add(panel);
         }
-        add(container);
+        add(calendar, BorderLayout.CENTER);
     }
     
     /**
