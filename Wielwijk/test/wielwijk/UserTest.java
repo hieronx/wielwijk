847e5af34c879b29a72393eb910af3aa69afa3de
Bart Slangewal <bartslangewal@hotmail.com>
2012-12-12 16:48:45 +0100

    UserTest compleet werkend

--- Wielwijk/test/wielwijk/UserTest.java
+++ Wielwijk/test/wielwijk/UserTest.java
@@ -2,29 +2,40 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package wielwijk;
 
+import java.util.HashMap;
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
  *
- * @author ylverhoog
+ * @author Bart
  */
 public class UserTest {
-
+    
     public UserTest() {
     }
-
+    
     @BeforeClass
-    public static void setUpClass() throws Exception {
+    public static void setUpClass() {
     }
-
+    
     @AfterClass
-    public static void tearDownClass() throws Exception {
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        Wielwijk.getDBConnection();
+    }
+    
+    @After
+    public void tearDown() {
     }
 
     /**
@@ -33,10 +44,25 @@ public class UserTest {
     @Test
     public void testSuspend() {
         System.out.println("suspend");
-        User instance = null;
-        instance.suspend();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //Maakt een nieuwe user
+        UserContainer.addMember("bla", "bla", 1, "bla", "2012-12-12");
+        //Haalt hoogste id op, oftewel de zojuist gemaakte usSELECT * FROM users WHERE id IN SELECT MAX ID FROM userser
+        java.util.List results = Wielwijk.db.query("SELECT * FROM users WHERE id IN (SELECT MAX( id )FROM users)");
+        java.util.Map<String, Object> map = (HashMap<String, Object>) results.get(0);
+        int id = Integer.parseInt(String.valueOf((Long)map.get("id")));
+        //suspend de user
+        UserContainer.getUserById(id).suspend();
+        //kijk of de user inderdaad niet actief is
+        java.util.List results2 = Wielwijk.db.query("SELECT * FROM users WHERE id="+ id);
+        java.util.Map<String, Object> map2 = (HashMap<String, Object>) results2.get(0);
+        Boolean active = (Boolean)map2.get("active");
+        System.out.println("active " + active);
+        System.out.println("id " + id);
+        assertEquals(active,false);
+        
+        //verwijder de user weer
+        UserContainer.removeUser(id);
+        
     }
 
     /**
@@ -44,11 +70,24 @@ public class UserTest {
      */
     @Test
     public void testUnsuspend() {
-        System.out.println("unsuspend");
-        User instance = null;
-        instance.unsuspend();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //Maakt een nieuwe user
+        UserContainer.addMember("bla", "bla", 1, "bla", "2012-12-12");
+        //Haalt hoogste id op, oftewel de zojuist gemaakte usSELECT * FROM users WHERE id IN SELECT MAX ID FROM userser
+        java.util.List results = Wielwijk.db.query("SELECT * FROM users WHERE id IN (SELECT MAX( id )FROM users)");
+        java.util.Map<String, Object> map = (HashMap<String, Object>) results.get(0);
+        int id = Integer.parseInt(String.valueOf((Long)map.get("id")));
+        //unsuspend de user
+        UserContainer.getUserById(id).unsuspend();
+        //kijk of de user inderdaad niet actief is
+        java.util.List results2 = Wielwijk.db.query("SELECT * FROM users WHERE id="+ id);
+        java.util.Map<String, Object> map2 = (HashMap<String, Object>) results2.get(0);
+        Boolean active = (Boolean)map2.get("active");
+        System.out.println("active " + active);
+        System.out.println("id " + id);
+        assertEquals(active,true);
+        
+        //verwijder de user weer
+        UserContainer.removeUser(id);
     }
 
     /**
@@ -57,12 +96,23 @@ public class UserTest {
     @Test
     public void testGetId() {
         System.out.println("getId");
-        User instance = null;
-        int expResult = 0;
-        int result = instance.getId();
+        //Maakt een nieuwe user
+        UserContainer.addMember("bla", "bla", 1, "bla", "2012-12-12");
+        //Haalt hoogste id op, oftewel de zojuist gemaakte usSELECT * FROM users WHERE id IN SELECT MAX ID FROM userser
+        java.util.List results = Wielwijk.db.query("SELECT * FROM users WHERE id IN (SELECT MAX( id )FROM users)");
+        java.util.Map<String, Object> map = (HashMap<String, Object>) results.get(0);
+        int id = Integer.parseInt(String.valueOf((Long)map.get("id")));
+        //De test van de getId functie
+        User test=UserContainer.getUserById(id);
+        long result = test.getId();
+        //verwacht resultaat uit de database
+        java.util.List results2 = Wielwijk.db.query("SELECT * FROM users WHERE id="+ id);
+        java.util.Map<String, Object> map2 = (HashMap<String, Object>) results2.get(0);
+        long expResult = (Long)map2.get("id");
+        //vergelijking
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        // wis de user weer
+        UserContainer.removeUser(id);
     }
 
     /**
@@ -70,27 +120,71 @@ public class UserTest {
      */
     @Test
     public void testGetName() {
-        System.out.println("getName");
-        User instance = null;
-        String expResult = "";
-        String result = instance.getName();
+        //Maakt een nieuwe user
+        UserContainer.addMember("bla", "bla", 1, "bla", "2012-12-12");
+        //Haalt hoogste id op, oftewel de zojuist gemaakte user
+        java.util.List results = Wielwijk.db.query("SELECT * FROM users WHERE id IN (SELECT MAX( id )FROM users)");
+        java.util.Map<String, Object> map = (HashMap<String, Object>) results.get(0);
+        int id = Integer.parseInt(String.valueOf((Long)map.get("id")));
+        //De test van de getName functie
+        User test=UserContainer.getUserById(id);
+        String result = test.getName();
+        //verwacht resultaat uit de database
+        java.util.List results2 = Wielwijk.db.query("SELECT * FROM users WHERE id="+ id);
+        java.util.Map<String, Object> map2 = (HashMap<String, Object>) results2.get(0);
+        String expResult = (String)map2.get("name");
+        //vergelijking
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //wis de user weer
+        UserContainer.removeUser(id);
     }
 
     /**
-     * Test of getBirthdate method, of class User.
+     * Test of getPassword method, of class User.
      */
     @Test
-    public void testGetBirthdate() {
-        System.out.println("getBirthdate");
-        User instance = null;
-        String expResult = "";
-        String result = instance.getBirthdate();
+    public void testGetPassword() {
+        System.out.println("getPassword");
+        //maakt test user
+        UserContainer.addMember("bla", "bla", 1, "bla", "2012-12-12");
+        //Haalt hoogste id op, oftewel de zojuist gemaakte user 
+        java.util.List results = Wielwijk.db.query("SELECT * FROM users WHERE id IN (SELECT MAX( id )FROM users)");
+        java.util.Map<String, Object> map = (HashMap<String, Object>) results.get(0);
+        int id = Integer.parseInt(String.valueOf((Long)map.get("id")));
+        //De test van de getPassword functie
+        User test=UserContainer.getUserById(id);
+        String result = test.getPassword();
+        //verwacht resultaat uit de database
+        java.util.List results2 = Wielwijk.db.query("SELECT * FROM users WHERE id="+ id);
+        java.util.Map<String, Object> map2 = (HashMap<String, Object>) results2.get(0);
+        String expResult = (String)map2.get("password");
+        //vergelijking
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        UserContainer.removeUser(id);
+    }
+
+    /**
+     * Test of getAddress method, of class User.
+     */
+    @Test
+    public void testGetAddress() {
+        System.out.println("getAddress");
+        //maakt test user
+        UserContainer.addMember("bla", "bla", 1, "bla", "2012-12-12");
+        //Haalt hoogste id op, oftewel de zojuist gemaakte user 
+        java.util.List results = Wielwijk.db.query("SELECT * FROM users WHERE id IN (SELECT MAX( id )FROM users)");
+        java.util.Map<String, Object> map = (HashMap<String, Object>) results.get(0);
+        int id = Integer.parseInt(String.valueOf((Long)map.get("id")));
+        //De test van de getAdress functie
+        User test=UserContainer.getUserById(id);
+        String result = test.getAddress();
+        //verwacht resultaat uit de database
+        java.util.List results2 = Wielwijk.db.query("SELECT * FROM users WHERE id="+ id);
+        java.util.Map<String, Object> map2 = (HashMap<String, Object>) results2.get(0);
+        String expResult = (String)map2.get("address");
+        //vergelijking
+        assertEquals(expResult, result);
+        UserContainer.removeUser(id);
     }
 
     /**
@@ -99,12 +193,22 @@ public class UserTest {
     @Test
     public void testGetPicture() {
         System.out.println("getPicture");
-        User instance = null;
-        int expResult = 0;
-        int result = instance.getPicture();
+        //maakt test user
+        UserContainer.addMember("bla", "bla", 1, "bla", "2012-12-12");
+        //Haalt hoogste id op, oftewel de zojuist gemaakte user 
+        java.util.List results = Wielwijk.db.query("SELECT * FROM users WHERE id IN (SELECT MAX( id )FROM users)");
+        java.util.Map<String, Object> map = (HashMap<String, Object>) results.get(0);
+        int id = Integer.parseInt(String.valueOf((Long)map.get("id")));
+        //De test van de getPicture functie
+        User test=UserContainer.getUserById(id);
+        int result = test.getPicture();
+        //verwacht resultaat uit de database
+        java.util.List results2 = Wielwijk.db.query("SELECT * FROM users WHERE id="+ id);
+        java.util.Map<String, Object> map2 = (HashMap<String, Object>) results2.get(0);
+        int expResult = (Integer)map2.get("picture");
+        //vergelijking
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        UserContainer.removeUser(id);
     }
 
     /**
@@ -113,12 +217,22 @@ public class UserTest {
     @Test
     public void testIsActive() {
         System.out.println("isActive");
-        User instance = null;
-        boolean expResult = false;
-        boolean result = instance.isActive();
+        //maakt test user
+        UserContainer.addMember("bla", "bla", 1, "bla", "2012-12-12");
+        //Haalt hoogste id op, oftewel de zojuist gemaakte user 
+        java.util.List results = Wielwijk.db.query("SELECT * FROM users WHERE id IN (SELECT MAX( id )FROM users)");
+        java.util.Map<String, Object> map = (HashMap<String, Object>) results.get(0);
+        int id = Integer.parseInt(String.valueOf((Long)map.get("id")));
+        //De test van de isActive functie
+        User test=UserContainer.getUserById(id);
+        boolean result = test.isActive();
+        //verwacht resultaat uit de database
+        java.util.List results2 = Wielwijk.db.query("SELECT * FROM users WHERE id="+ id);
+        java.util.Map<String, Object> map2 = (HashMap<String, Object>) results2.get(0);
+        boolean expResult = (Boolean)map2.get("active");
+        //vergelijking
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        UserContainer.removeUser(id);
     }
 
     /**
@@ -127,12 +241,45 @@ public class UserTest {
     @Test
     public void testIsBoard() {
         System.out.println("isBoard");
-        User instance = null;
-        boolean expResult = false;
-        boolean result = instance.isBoard();
+        //maakt test user
+        UserContainer.addMember("bla", "bla", 1, "bla", "2012-12-12");
+        //Haalt hoogste id op, oftewel de zojuist gemaakte user 
+        java.util.List results = Wielwijk.db.query("SELECT * FROM users WHERE id IN (SELECT MAX( id )FROM users)");
+        java.util.Map<String, Object> map = (HashMap<String, Object>) results.get(0);
+        int id = Integer.parseInt(String.valueOf((Long)map.get("id")));
+        //De test van de isBoard functie
+        User test=UserContainer.getUserById(id);
+        boolean result = test.isBoard();
+        //verwacht resultaat uit de database
+        java.util.List results2 = Wielwijk.db.query("SELECT * FROM users WHERE id="+ id);
+        java.util.Map<String, Object> map2 = (HashMap<String, Object>) results2.get(0);
+        boolean expResult = (Boolean)map2.get("board");
+        //vergelijking
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        UserContainer.removeUser(id);
     }
 
+    /**
+     * Test of toString method, of class User.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        //maakt test user
+        UserContainer.addMember("bla", "bla", 1, "bla", "2012-12-12");
+        //Haalt hoogste id op, oftewel de zojuist gemaakte user 
+        java.util.List results = Wielwijk.db.query("SELECT * FROM users WHERE id IN (SELECT MAX( id )FROM users)");
+        java.util.Map<String, Object> map = (HashMap<String, Object>) results.get(0);
+        int id = Integer.parseInt(String.valueOf((Long)map.get("id")));
+        //De test van de isBoard functie
+        User test=UserContainer.getUserById(id);
+        String result = test.toString();
+        //verwacht resultaat uit de database
+        java.util.List results2 = Wielwijk.db.query("SELECT * FROM users WHERE id="+ id);
+        java.util.Map<String, Object> map2 = (HashMap<String, Object>) results2.get(0);
+        String expResult = (String)map2.get("name");
+        //vergelijking
+        assertEquals(expResult, result);
+        UserContainer.removeUser(id);
+    }
 }
