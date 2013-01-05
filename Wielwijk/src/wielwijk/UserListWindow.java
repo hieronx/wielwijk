8d9a3bd08d0721606670c74b7b4856a9a5f65f95
Jeroen Offerijns <jeroen@offerijns.nl>
2013-01-05 19:11:47 +0100

    Gebruikerslijst weer toegevoegd (was per ongeluk gecement)

--- Wielwijk/src/wielwijk/UserListWindow.java
+++ Wielwijk/src/wielwijk/UserListWindow.java
@@ -24,26 +24,26 @@ public class UserListWindow {
     JPanel container = new JPanel();
     container.setLayout(new GridLayout(3, 2));
 
-//    java.util.List res = Wielwijk.db.query("SELECT * FROM users");
-//    
-//    String usernames = "USERS\n";
-//    for (int i = 0; i < res.size(); i++) {
-//        Map<String, Object> map = (HashMap<String, Object>) res.get(i);
-//        
-//        panel = new JPanel(new BorderLayout());
-//        
-//        layout = new JPanel();
-//        layout.add(Box.createRigidArea(new Dimension(0, 50)));
-//        label = new JLabel();
-//        label.setText((String) map.get("name"));
-//        layout.add(label);
-//        panel.add(layout, BorderLayout.NORTH);
-//        
-//        JLabel iconLabel = new JLabel(new ImageIcon("pic.jpg"));
-//        panel.add(iconLabel, BorderLayout.CENTER);
-//        
-//        container.add(panel);
-//    }
+    java.util.List res = Wielwijk.db.query("SELECT * FROM users");
+    
+    String usernames = "USERS\n";
+    for (int i = 0; i < res.size(); i++) {
+        Map<String, Object> map = (HashMap<String, Object>) res.get(i);
+        
+        panel = new JPanel(new BorderLayout());
+        
+        layout = new JPanel();
+        layout.add(Box.createRigidArea(new Dimension(0, 50)));
+        label = new JLabel();
+        label.setText((String) map.get("name"));
+        layout.add(label);
+        panel.add(layout, BorderLayout.NORTH);
+        
+        JLabel iconLabel = new JLabel(new ImageIcon("pic.jpg"));
+        panel.add(iconLabel, BorderLayout.CENTER);
+        
+        container.add(panel);
+    }
     
     Wielwijk.gui.addElement(window_id, container);
   }
